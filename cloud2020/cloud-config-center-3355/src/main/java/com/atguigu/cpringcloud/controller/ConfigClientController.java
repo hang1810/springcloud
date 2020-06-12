1bb8a05ceef4ab2118965cbf4dc8b01c2124c42f
Hang <137628022@qq.com>
2020-06-12 17:39:59 +0800

    12

---
+++ cloud2020/cloud-config-center-3355/src/main/java/com/atguigu/cpringcloud/controller/ConfigClientController.java
@@ -0,0 +1,17 @@
+package com.atguigu.cpringcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @RequestMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
