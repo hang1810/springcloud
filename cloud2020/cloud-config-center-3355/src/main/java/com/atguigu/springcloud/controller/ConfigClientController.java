9a263700eb5d2f0a5b7faef5aad0a0c0e325317c
Hang <137628022@qq.com>
2020-06-13 15:13:23 +0800

    a

--- cloud2020/cloud-config-center-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud2020/cloud-config-center-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -1,4 +1,4 @@
-package com.atguigu.cpringcloud.controller;
+package com.atguigu.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
