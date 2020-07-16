22693d8b9401e6e59ee8047c549f51c5d175bdc8
Hang <137628022@qq.com>
2020-07-16 13:44:10 +0800

    HANG 0716

--- cloud2020/seata-order-service2001/src/main/java/com/atguigu/springcloud/service/impl/OrderServiceImpl.java
+++ cloud2020/seata-order-service2001/src/main/java/com/atguigu/springcloud/service/impl/OrderServiceImpl.java
@@ -35,23 +35,23 @@ public class OrderServiceImpl implements OrderService {
     @GlobalTransactional(name = "fsp-create-order",rollbackFor = Exception.class)
     public void create(Order order) {
 
-        log.info("-->开始新建订单");
-        orderDao.create(order);
-
-
-        log.info("-->订单微服务开始调用库存，做扣减");
-        storageService.decrease(order.getProductId(),order.getCount());
-        log.info("-->订单微服务开始调用库存，做扣减end");
-
-        log.info("-->订单微服务开始调用账户，做扣减");
-        accountService.decrease(order.getUserId(),order.getCount());
-        log.info("-->订单微服务开始调用账户，做扣减end");
-
-
-
-        //4修改订单状态，从0-->1 代表完成
-        log.info("-->订单微服务开始调用账户，做扣减begin");
-        orderDao.update(order.getUserId(),0);
-        log.info("-->订单结束");
+//        log.info("-->开始新建订单");
+//        orderDao.create(order);
+//
+//
+//        log.info("-->订单微服务开始调用库存，做扣减");
+//        storageService.decrease(order.getProductId(),order.getCount());
+//        log.info("-->订单微服务开始调用库存，做扣减end");
+//
+//        log.info("-->订单微服务开始调用账户，做扣减");
+//        accountService.decrease(order.getUserId(),order.getCount());
+//        log.info("-->订单微服务开始调用账户，做扣减end");
+//
+//
+//
+//        //4修改订单状态，从0-->1 代表完成
+//        log.info("-->订单微服务开始调用账户，做扣减begin");
+//        orderDao.update(order.getUserId(),0);
+//        log.info("-->订单结束");
     }
 }
